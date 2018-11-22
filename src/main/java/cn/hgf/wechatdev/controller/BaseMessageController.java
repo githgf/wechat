2d4390976f95ee518542dd99ddf06e29f2b28abf
linsen <linsen@123feng.com>
2018-11-22 18:03:28 +0800

    update pom

--- src/main/java/cn/hgf/wechatdev/controller/BaseMessageController.java
+++ src/main/java/cn/hgf/wechatdev/controller/BaseMessageController.java
@@ -1,6 +1,5 @@
 package cn.hgf.wechatdev.controller;
 
-import cn.hgf.wechatdev.common.CommonParam;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
