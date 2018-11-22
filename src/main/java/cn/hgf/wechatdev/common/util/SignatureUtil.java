209086ac259995954a089c885b42999d6a2ae4db
linsen <linsen@123feng.com>
2018-11-22 20:09:43 +0800

    commit

---
+++ src/main/java/cn/hgf/wechatdev/common/util/SignatureUtil.java
@@ -0,0 +1,31 @@
+package cn.hgf.wechatdev.common.util;
+
+import cn.hgf.wechatdev.common.CommonParam;
+import org.apache.commons.codec.digest.DigestUtils;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class SignatureUtil {
+
+    public static  boolean checkSignature(String timestamp,String nonce,String signature){
+
+        List<String> checkList = new ArrayList<>();
+        checkList.add(CommonParam.WECHAT_SERVER_KEY);
+        checkList.add(nonce);
+        checkList.add(timestamp);
+        Collections.sort(checkList);
+        String sha1Hex = DigestUtils.sha1Hex(checkList.get(0) + checkList.get(1) + checkList.get(2));
+        if (signature.equals(sha1Hex)){
+            return true;
+        }
+        return false;
+    }
+    public static void main(String[] args){
+
+        checkSignature("1542885977","1722755572","d836e81fd71e7f7e8312ce900eebef64749591c0");
+
+    }
+
+}
